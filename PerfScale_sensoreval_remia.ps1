a4323f3d041f29bc97df7da9065e826c6d8ca3b1
Mark Silvey <39961687+silveyy@users.noreply.github.com>
2020-06-25 11:14:14 -0700

    Automated commit

---
+++ PerfScale_sensoreval_remia.ps1
@@ -0,0 +1,15 @@
+﻿# Added by ermatt 2/21/2020 for remia
+
+#Force Sensor evaluation action on clients
+
+$strAction = "C4EAA67D-DD9A-0000-0000-000000000000"
+try {
+Invoke-WmiMethod -ComputerName $env:computername -Namespace root\ccm -Class SMS_Client -Name TriggerSchedule -ArgumentList $strAction -ErrorAction Stop | Out-Null
+write-host "Sensor Triggered"
+}
+catch {
+write-host "$env:computername`: $_" -ForegroundColor Red
+}
+
+
+# Expected return string: Sensor Triggered
