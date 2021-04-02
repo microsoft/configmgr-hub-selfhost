b6f319dc439ba83f7d615f627d5317b4021fb817
sihlamMSFT <62903638+sihlamMSFT@users.noreply.github.com>
2021-04-02 14:54:26 -0700

    Create ConsoleExtensionValidationRunner.ps1

---
+++ tools/ConsoleExtensionValidationRunner.ps1
@@ -0,0 +1,45 @@
+# ===================================================================
+#
+#   Main entry point.
+#
+# ===================================================================
+
+function Main
+{
+    $artifactsLocation = $Env:SYSTEM_ARTIFACTSDIRECTORY;
+    $consoleExValidatorLocation = $artifactsLocation + "\lib\net40\Microsoft.ConfigurationManager.ConsoleExtensionCommon.dll";
+    $itemsRootDirectory = $Env:BUILD_REPOSITORY_LOCALPATH;
+    $consoleExsDirectory = $itemsRootDirectory + "\" + "objects\consoleextension";
+    $cabFilesToValidated = Get-ChildItem $consoleExsDirectory;
+
+
+    #Initialize objects
+    [Reflection.Assembly]::LoadFile($consoleExValidatorLocation)
+    $objectFactory = new-object Microsoft.ConfigurationManager.ConsoleExtension.SystemFunctions.SystemObjectFactory
+    $validator = new-object -TypeName Microsoft.ConfigurationManager.ConsoleExtension.ConsoleExtensionValidator -ArgumentList $objectFactory
+
+    #Start validation
+    foreach($file in $cabFilesToValidated)
+    {
+        Try
+        {
+            $expandedCabLocation = [System.IO.Path]::GetFileNameWithoutExtension($file);
+            $validator.VerifyExtensionCabSigniture($file);
+            $validator.VerifyExtensionCabContent($consoleExsDirectory + "\" + "expandedCabLocation")
+        }
+        Catch
+        {
+            $ErrorMessage = $_.Exception.Message;
+            Write-Error "Validation failed with " + $ErrorMessage;
+        }
+    }
+}
+
+
+Write-host 'Running console extension validation...'
+Write-Host "=================================================="
+
+Main;
+
+Write-Host "Console extension validation finished.";
+Write-Host "=================================================="
