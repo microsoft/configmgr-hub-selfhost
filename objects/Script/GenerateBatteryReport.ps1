a11df6f40862053e43701698c21e86919fb80dc1
Mark Silvey <39961687+silveyy@users.noreply.github.com>
2021-04-23 15:16:56 -0700

    Automated commit

---
+++ objects/Script/GenerateBatteryReport.ps1
@@ -0,0 +1,4 @@
+﻿$BatteryReportXML = '\\scfs\users\jyhlin\dataninja\power\BatteryReports\'+(Get-CIMInstance CIM_ComputerSystem).Name+'.xml'
+powercfg /batteryreport /xml /output $BatteryReportXML
+$BatteryReportHTML = '\\scfs\users\jyhlin\dataninja\power\BatteryReports\'+(Get-CIMInstance CIM_ComputerSystem).Name+'.html'
+powercfg /batteryreport /output $BatteryReportHTML
